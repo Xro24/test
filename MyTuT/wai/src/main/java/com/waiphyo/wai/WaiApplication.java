41c601e3e543bc82e8604ea018a9282164c7b76a
DESKTOP-VEUQUHV\User <wp67636@gmail.com>
2023-03-05 11:00:08 +0630

    first commit

---
+++ MyTuT/wai/src/main/java/com/waiphyo/wai/WaiApplication.java
@@ -0,0 +1,13 @@
+package com.waiphyo.wai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaiApplication.class, args);
+    }
+
+}
